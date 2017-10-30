d9471c90e14657fa1fd6829efec0b4251379bb79
Benjamin ALvarez <benjaminao@gmail.com>
2017-10-30 14:26:35 -0400

    fist deploy

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
